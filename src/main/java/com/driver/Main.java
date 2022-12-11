34e2808a8edacccd613dfe877115cf53bc7bd51f
Rahul Raj <rahulfrndz02@gmail.com>
2022-12-11 21:13:23 +0530

    test

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println(p.product(3,4,5));
         System.out.println(p.product(6.6,7.7));
     }
-    static class Product{
+    public static class Product{
         public int product(int x, int y){
                 return x*y;
         }
